bf74054f37312152c60a142af9add8f49f3ac386
sunil10790play <sunil10790play@gmail.com>
2022-05-17 12:43:36 +0530

    Add migrations and migrations script

--- app.py
+++ app.py
@@ -3,21 +3,23 @@ from os import abort
 import sys
 from flask import Flask, jsonify, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:integrity@localhost:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo id: {self.id}, description: {self.description}>'
 
-db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
